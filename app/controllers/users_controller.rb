23ab7cbce61fb01dcdc16e169155edbcb6b812e9
Travis T <travistokuyama@gmail.com>
2018-11-27 18:02:43 +0000

    add login and signup erb and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  get '/login' do
+    erb :login
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/users' do
+    @user = User.new(params[:username], params[:email], params[:password_digest])
+    redirect '/users'
+  end
 end
